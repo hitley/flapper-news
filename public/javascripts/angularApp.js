97d9c924ae602cf0c308173508e4ff308d01a624
hitley <hiteshbechar@Hiteshs-iMac.local>
2016-01-06 22:49:55 +0800

    angular and node services all wired up to each other

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,9 +1,46 @@
 var app = angular.module('flapperNews', ['ui.router']);
 
-app.factory('posts', [function() {
+app.factory('posts', ['$http', function($http) {
 	var o = {
 		posts: []
 	};
+
+	o.getAll = function() {
+		return $http.get('/posts').success(function(data) {
+			angular.copy(data, o.posts);
+		});
+	};
+
+	o.create = function(post) {
+		return $http.post('/posts', post).success(function(data) { 
+			o.posts.push(data);
+		});
+	};
+
+	o.upvote = function(post) {
+		return $http.put('/posts/' + post._id + '/upvote').success(function(data) {
+			post.upvotes += 1;
+		});
+	};
+	
+	o.get = function(id) {
+		return $http.get('/posts/' + id).then(function(res) { 
+			return res.data;
+		});
+	};
+
+	o.addComment = function(post, comment) {
+		return $http.post('/posts/' + post._id + '/comments', comment).success(function(data) {
+			post.comments.push(data);
+		});
+	};
+
+	o.upvoteComment = function(post, comment) {
+		return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote').success(function(data) { 
+			comment.upvotes += 1;
+		});
+	};
+
 	return o;
 }]);
 
@@ -19,14 +56,9 @@ app.controller('MainCtrl', [
 			// prevent empty posts
 			if (!$scope.title || $scope.title === '') {return;}
 
-			$scope.posts.push({
+			posts.create({
 				title: $scope.title,
 				link: $scope.link,
-				upvotes: 0,
-				comments: [
-					{author: 'Joe', body: 'Cool post!', upvotes: 0},
-					{author: 'Bob', body: 'Great idea but everything is wrong', upvotes: 0}
-				]
 			});
 			
 			$scope.title = '';
@@ -34,33 +66,34 @@ app.controller('MainCtrl', [
 		};
 
 		$scope.incrementUpvotes = function(post) {
-			post.upvotes += 1;
+			posts.upvote(post);
 		};
 	}
 ]);
 
 app.controller('PostsCtrl', [
 	'$scope',
-	'$stateParams',
 	'posts',
-	function($scope, $stateParams, posts) {
-		$scope.post = posts.posts[$stateParams.id];
+	'post',
+	function($scope, posts, post) {
+		$scope.post = post;
 
 		$scope.addComment = function() {
 
 			//prevent empty comments
 			if (!$scope.body || $scope.body === '') {return;}
 
-			$scope.post.comments.push({
+			posts.addComment(post, {
 				author: 'user', 
 				body: $scope.body,
 				upvotes: 0
 			});
+
 			$scope.body = '';
 		};
 
 		$scope.incrementUpvotes = function(comment) {
-			comment.upvotes += 1;
+			posts.upvoteComment(post, comment);
 		};
 	}
 ]);
@@ -73,15 +106,26 @@ app.config([
 			.state('home', {
 				url: '/home',
 				templateUrl: '/home.html',
-				controller: 'MainCtrl'
+				controller: 'MainCtrl',
+				resolve: {
+					postPromise: ['posts', function(posts) {
+						return posts.getAll();
+					}]
+				}
 		});
 
 		$stateProvider
 			.state('posts', {
 				url: '/posts/{id}',
 				templateUrl: '/posts.html',
-				controller: 'PostsCtrl'
+				controller: 'PostsCtrl',
+				resolve: {
+					post: ['$stateParams', 'posts', function($stateParams, posts) {
+						return posts.get($stateParams.id);
+					}]
+				}
 		});
 
 		$urlRouterProvider.otherwise('home');
 }]);
+
